3f0cbffb9f262e9c02c7044c099245406e1d664c
Md Johirul Islam <tusar8544@gmail.com>
2025-10-22 10:11:31 +0600

    button section complite

--- scripts/video.js
+++ scripts/video.js
@@ -6,9 +6,24 @@ const loadCategories = () =>{
  .then((error) => console.log(error))
 
 };
+// category
+// : 
+// "Music"
+// category_id
+// : 
+// "1001"
 
 //  display categories
-const displayCategories= () =>{
-    console.log(data);
+const displayCategories= (categories) =>{
+    const categoryContainer = document.getElementById("categroies");
+    categories.forEach((item) =>{
+        console.log(categories)
+        // create btn
+        const button = document.createElement("button");
+        button.classList = "btn";
+        button.innerText = item.category;
+        // add btn to categroy container
+        categoryContainer.append(button);
+    });
 };
 loadCategories()
