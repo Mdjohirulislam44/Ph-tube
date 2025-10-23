c3b9ab9ef3cce250ed9db424a0e1f0e355596f2d
Md Johirul Islam <tusar8544@gmail.com>
2025-10-23 19:26:17 +0600

    updated something

---
+++ scripts/test.js
@@ -0,0 +1,12 @@
+const isVerifyed = "";
+
+// if(isVerifyed === true){
+//     console.log("user is verify");
+
+// }else{
+//      console.log("user is not verify");
+// }
+
+console.log(`${isVerifyed === true ? "user is verify":"user is not verify"}`);
+
+<img class =" w-5" src = "https://img.icons8.com/?size=96&id=D9RtvkuOe31p&format=png"/>
